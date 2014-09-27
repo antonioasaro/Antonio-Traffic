a07ebd0d3343a0433a5da75ce1bbb4e5a8946fc8
antonioasaro <antonio_asaro@hotmail.com>
2014-09-27 14:34:16 -0400

    It all works. Now to put it on the server.

--- server/traffic.php
+++ server/traffic.php
@@ -2,14 +2,19 @@
 
   $cmd = 'test';
   $num = '4165621384';
-  $msg = '401 - 38mins; 407 - 29mins';
+  $msg = '';
   
   $sub = "Traffic to work";
   $frm = "pebble@traffic.php";
   $hdr = "From: ".$frm;
 
-  $json  = file_get_contents('https://maps.googleapis.com/maps/api/directions/json?origin=10+Fawnridge+Trail,+Scarborough&destination=1+commerce+valley+dr+markham&units=metric', 0, null, null);
+  $json = file_get_contents('https://maps.googleapis.com/maps/api/directions/json?origin=10+Fawnridge+Trail,+Scarborough&destination=1+commerce+valley+dr+markham&units=metric', 0, null, null);
   $traffic = process_traffic($json);
+  $msg = "401: " . $traffic;
+
+  $json = file_get_contents('https://maps.googleapis.com/maps/api/directions/json?origin=10+Fawnridge+Trail,+Scarborough&destination=1+commerce+valley+dr+markham&waypoints=via:43.892649,%20-79.186464&units=metric', 0, null, null);
+  $traffic = process_traffic($json);
+  $msg = $msg . "; 407: " . $traffic;
 
   $sentOK = 0;
   if (strcmp($cmd, "send") == 0) {
@@ -28,11 +33,10 @@
 function process_traffic($json_in) {
     $json_out = json_decode($json_in);
     if (!$json_out) die(); 
-//print $json_in;
     $distance = $json_out->routes[0]->legs[0]->distance->text;
-print $duration;
+    $duration = $json_out->routes[0]->legs[0]->duration->text;
 
-    $result = "abc";
+    $result = $distance . " - " . $duration;
     return $result;
 }
 
