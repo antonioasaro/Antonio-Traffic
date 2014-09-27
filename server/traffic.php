172a3f8dc2afde1db36b1423e55dbde0076a86fe
antonioasaro <antonio_asaro@hotmail.com>
2014-09-27 13:25:32 -0400

    .

--- server/traffic.php
+++ server/traffic.php
@@ -1,16 +1,5 @@
 <?PHP
 
-/***************************************
-  if (!isset($_GET['cmd'])) die();
-  if (!isset($_GET['frm'])) die();
-  if (!isset($_GET['num'])) die();
-  if (!isset($_GET['msg'])) die();
-
-  $cmd = $_GET['cmd'];
-  $frm = $_GET['frm'];
-  $num = $_GET['num'];
-  $msg = $_GET['msg'];
-***************************************/
   $cmd = 'test';
   $num = '4165621384';
   $msg = '401 - 38mins; 407 - 29mins';
@@ -18,6 +7,13 @@
   $sub = "Traffic to work";
   $frm = "pebble@traffic.php";
   $hdr = "From: ".$frm;
+
+  print "try json";
+  $json  = file_get_contents('https://maps.googleapis.com/maps/api/directions/json?origin=10+Fawnridge+Trail,+Scarborough&destination=1+commerce+valley+dr+markham&units=metric');
+  print "process json";
+//  $traffic = process_traffic($json);
+print "done processing json";
+//  print json_encode($traffic);
 
   $sentOK = 0;
   if (strcmp($cmd, "send") == 0) {
@@ -31,5 +27,44 @@
   $result[1] = array('I', $sentOK);
   print json_encode($result);
 
+
+/*************************************************/
+function curl_get($url){
+    if (!function_exists('curl_init')){
+        die('Sorry cURL is not installed!');
+    }
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_REFERER, $url);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/1.0");
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+
+function process_traffic($json_in) {
+    $json_output = json_decode(utf8_decode($json_in));
+    if (!$json_output) die(); 
+
+    $result = "abc";
+/*    $traffic     = $json_output->traffic;
+    $temp        = $json_output->main->temp;
+    $icon        = $traffic[0]->icon;
+
+    $result    = array();
+    $result[1] = $icon;
+    $result[2] = array('I', round($temp, 0));
+*/
+    return $result;
+}
+
+/***************************************************************
+https://maps.googleapis.com/maps/api/directions/json?origin=10+Fawnridge+Trail,+Scarborough&destination=1+commerce+valley+dr+markham&units=metric
+
+https://maps.googleapis.com/maps/api/directions/json?origin=10+Fawnridge+Trail,+Scarborough&destination=1+commerce+valley+dr+markham&waypoints=via:43.892649,%20-79.186464&units=metric
+***************************************************************/
 ?>
 
