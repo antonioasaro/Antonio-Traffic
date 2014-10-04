5eb3d1fd2483482bd8a781923a151d3ca49dcc2d
antonioasaro <antonio_asaro@hotmail.com>
2014-10-03 21:32:19 -0400

    use mapquest

--- server/traffic.php
+++ server/traffic.php
@@ -8,13 +8,15 @@
   $frm = "pebble@traffic.php";
   $hdr = "From: ".$frm;
 
+  $json = file_get_contents('http://www.mapquestapi.com/directions/v2/route?key=Fmjtd%7Cluurnu6bnd%2Cr5%3Do5-9wbggr&outFormat=json&routeType=fastest&timeType=1&locale=en_US&unit=k&from=10%20Fawnridge%20Trail%20Scarborough%20canada&to=1%20commerce%20valley%20east%20markham%20canada', 0, null, null);
+  $traffic = process_traffic($json);
+  $msg = "Hwy 401: " . $traffic;
+
+/*
   $json = file_get_contents('https://maps.googleapis.com/maps/api/directions/json?origin=10+Fawnridge+Trail,+Scarborough&destination=1+commerce+valley+dr+markham&units=metric', 0, null, null);
   $traffic = process_traffic($json);
-  $msg = "401: " . $traffic;
-
-  $json = file_get_contents('https://maps.googleapis.com/maps/api/directions/json?origin=10+Fawnridge+Trail,+Scarborough&destination=1+commerce+valley+dr+markham&waypoints=via:43.892649,%20-79.186464&units=metric', 0, null, null);
-  $traffic = process_traffic($json);
-  $msg = $msg . "; 407: " . $traffic;
+  $msg = $msg . "; Hwy 407: " . $traffic . ".";
+*/
 
   $sentOK = 0;
   if (strcmp($cmd, "send") == 0) {
@@ -33,14 +35,16 @@
 function process_traffic($json_in) {
     $json_out = json_decode($json_in);
     if (!$json_out) die(); 
-    $distance = $json_out->routes[0]->legs[0]->distance->text;
-    $duration = $json_out->routes[0]->legs[0]->duration->text;
+    $distance = $json_out->route->distance;
+    $duration = $json_out->route->realTime;
 
-    $result = $distance . " - " . $duration;
-    return $result;
+    $results = ceil($distance*10)/10 . " km - " . ceil($duration/60*10)/10 . " mins";
+    return $results;
 }
 
 /***************************************************************
+http://www.mapquestapi.com/directions/v2/route?key=Fmjtd%7Cluurnu6bnd%2Cr5%3Do5-9wbggr&callback=renderAdvancedNarrative&ambiguities=ignore&avoidTimedConditions=false&doReverseGeocode=true&outFormat=json&routeType=fastest&timeType=1&enhancedNarrative=false&shapeFormat=raw&generalize=0&locale=en_US&unit=m&from=10%20Fawnridge%20Trail%20Scarborough%20canada&to=1%20commerce%20valley%20east%20markham%20canada
+
 https://maps.googleapis.com/maps/api/directions/json?origin=10+Fawnridge+Trail,+Scarborough&destination=1+commerce+valley+dr+markham&units=metric
 
 https://maps.googleapis.com/maps/api/directions/json?origin=10+Fawnridge+Trail,+Scarborough&destination=1+commerce+valley+dr+markham&waypoints=via:43.892649,%20-79.186464&units=metric
