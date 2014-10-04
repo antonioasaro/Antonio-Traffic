e5f4ce8f7285cd5da05957d879b39e0ebd3f110b
antonioasaro <antonio_asaro@hotmail.com>
2014-10-04 08:59:30 -0400

    Split 407 trip in two

--- server/traffic.php
+++ server/traffic.php
@@ -1,6 +1,6 @@
 <?PHP
 
-  $cmd = 'send';
+  $cmd = 'send'; // 'test';
   $num = '4165621384@sms.rogers.com';
   $msg = '';
   
@@ -8,15 +8,15 @@
   $frm = "pebble@traffic.php";
   $hdr = "From: ".$frm;
 
-  $json = file_get_contents('http://www.mapquestapi.com/directions/v2/route?key=Fmjtd%7Cluurnu6bnd%2Cr5%3Do5-9wbggr&outFormat=json&routeType=fastest&timeType=1&locale=en_US&unit=k&from=10%20Fawnridge%20Trail%20Scarborough%20canada&to=1%20commerce%20valley%20east%20markham%20canada', 0, null, null);
+  $json = file_get_contents('http://www.mapquestapi.com/directions/v2/route?key=Fmjtd%7Cluurnu6bnd%2Cr5%3Do5-9wbggr&outFormat=json&routeType=fastest&timeType=1&locale=en_US&unit=k&from=43.795434,-79.170746&to=43.839313,-79.379755', 0, null, null);
   $traffic = process_traffic($json);
-  $msg = "Hwy 401: " . $traffic;
+  $msg = "Hwy 401: " . $traffic[0] . " km - " . $traffic[1] . " mins";
 
-/*
-  $json = file_get_contents('https://maps.googleapis.com/maps/api/directions/json?origin=10+Fawnridge+Trail,+Scarborough&destination=1+commerce+valley+dr+markham&units=metric', 0, null, null);
+  $json = file_get_contents('http://www.mapquestapi.com/directions/v2/route?key=Fmjtd%7Cluurnu6bnd%2Cr5%3Do5-9wbggr&outFormat=json&routeType=fastest&timeType=1&locale=en_US&unit=k&from=43.795434,-79.170746&to=43.882876,%20-79.182163', 0, null, null);
+  $waypoint = process_traffic($json);
+  $json = file_get_contents('http://www.mapquestapi.com/directions/v2/route?key=Fmjtd%7Cluurnu6bnd%2Cr5%3Do5-9wbggr&outFormat=json&routeType=fastest&timeType=1&locale=en_US&unit=k&from=43.882876,-79.182163&to=43.839313,-79.379755', 0, null, null);
   $traffic = process_traffic($json);
-  $msg = $msg . "; Hwy 407: " . $traffic . ".";
-*/
+  $msg = $msg . "; Hwy 407: " .  ($waypoint[0]+$traffic[0]) . " km - " . ($waypoint[1]+$traffic[1]) . " mins.";
 
   $sentOK = 0;
   if (strcmp($cmd, "send") == 0) {
@@ -38,16 +38,9 @@ function process_traffic($json_in) {
     $distance = $json_out->route->distance;
     $duration = $json_out->route->realTime;
 
-    $results = ceil($distance*10)/10 . " km - " . ceil($duration/60*10)/10 . " mins";
+    $results = array(ceil($distance*10)/10, ceil($duration/60*10)/10);
     return $results;
 }
 
-/***************************************************************
-http://www.mapquestapi.com/directions/v2/route?key=Fmjtd%7Cluurnu6bnd%2Cr5%3Do5-9wbggr&callback=renderAdvancedNarrative&ambiguities=ignore&avoidTimedConditions=false&doReverseGeocode=true&outFormat=json&routeType=fastest&timeType=1&enhancedNarrative=false&shapeFormat=raw&generalize=0&locale=en_US&unit=m&from=10%20Fawnridge%20Trail%20Scarborough%20canada&to=1%20commerce%20valley%20east%20markham%20canada
-
-https://maps.googleapis.com/maps/api/directions/json?origin=10+Fawnridge+Trail,+Scarborough&destination=1+commerce+valley+dr+markham&units=metric
-
-https://maps.googleapis.com/maps/api/directions/json?origin=10+Fawnridge+Trail,+Scarborough&destination=1+commerce+valley+dr+markham&waypoints=via:43.892649,%20-79.186464&units=metric
-***************************************************************/
 ?>
 
